3b38930f02388345f59213e3a32844a781636072
Jack O'Sullivan <jackos1998@gmail.com>
2020-02-16 13:19:10 +0000

    Add initial YeeTF challenge

---
+++ web/yeetf/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+[ -z "$FLAG" ] && FLAG="$(cat /run/secrets/flag.txt)"
+sqlite3 CTFd/ctfd.db "UPDATE flags SET content = '$FLAG' WHERE id = 1;"
+
+exec gunicorn 'CTFd:create_app()' \
+    --bind '0.0.0.0:80' \
+    --workers 1 \
+    --worker-tmp-dir "/dev/shm" \
+    --worker-class "gevent"
