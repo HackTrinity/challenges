f46e0fad32f9bda775a346604ff471ba742d5e70
Jack O'Sullivan <jackos1998@gmail.com>
2020-03-13 15:17:17 +0000

    web/badmin_2: Fix installing flag on startup

--- web/badmin_2/challenge-entrypoint.sh
+++ web/badmin_2/challenge-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-[ -f "/run/secrets/flag.txt" ] && install -o badmin -g badmin -m 660 /run/secrets/flag.txt youwillnevergethisLALALALA.txt
+[ -f "/run/secrets/flag.txt" ] && install -o badmin -g badmin -m 640 /run/secrets/flag.txt /home/badmin/youwillnevergethisLALALALA.txt
 
 php-fpm7 --daemonize --allow-to-run-as-root
 exec nginx -g 'daemon off;'
